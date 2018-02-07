4f5f970d2671676a0449418d2a9b5d72ace09cac
Mike Pasek <mpasek05@gmail.com>
2018-02-08 09:30:45 +1100

    Added initial routing for database and Spotify authentication.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,55 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
+var wiredep = require('wiredep').stream;
+var inject = require('gulp-inject');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 
-gulp.task('jshint', () => {
-    gulp.src(jsFiles)
+gulp.task('style', () => {
+    return gulp.src(jsFiles)
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish', {
             verbose: true
-        }));
+        }))
+        .pipe(jscs());
 });
 
-/*gulp.task('default', ['watch']);
+gulp.task('inject', () => {
+    var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js'], {read: false});
+    
+    var injectOptions = {
+        ignorePath: '/public'
+    };
+    
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
+    };
+    
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./src/views'));
+});
 
-gulp.task('watch', () => {
-    gulp.watch(jsFiles, ['jshint']);
-});*/
+gulp.task('serve', ['style', 'inject'], () => {
+    var options = {
+        script: 'server.js',
+        delayTime: 1,
+        env: {
+            'PORT': 3000
+        },
+        watch: jsFiles
+    };
 
+    return nodemon(options)
+        .on('restart', (ev) => {
+            console.log('Restarting...');
+    });
+});
+
+/*
+gulp.task('default', ['style', 'inject']);*/
--- server.js
+++ server.js
@@ -2,14 +2,17 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyparser = require('body-parser');
 var cors = require('cors');
+var cookieParser = require('cookie-parser');
 
 var app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const URI = 'mongodb://mpasek:Shortstop5@cluster0-shard-00-00-utw0a.mongodb.net:27017,' +
-            'cluster0-shard-00-01-utw0a.mongodb.net:27017,' +
-            'cluster0-shard-00-02-utw0a.mongodb.net:27017' +
-            '/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+    'cluster0-shard-00-01-utw0a.mongodb.net:27017,' +
+    'cluster0-shard-00-02-utw0a.mongodb.net:27017' +
+    '/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+const authRouter = require('./src/routes/authRoutes');
+const dbRouter = require('./src/routes/dbRoutes');
 
 
 // Connect to mongodb
@@ -26,9 +29,18 @@ mongoose.connection.on('error', (err) => {
 });
 
 app.use(express.static('public'));
+/*app.use(express.static(__dirname + '/public'))
+    .use(cookieParser());*/
+app.use(express.static('src/views'));
+
+app.use(cors());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded());
+app.use('/', authRouter);
+app.use('/api', dbRouter);
 
 app.get('/', (req, res) => {
-    res.send('Hello World! Server and database running correctly.');
+    res.send('Hello World! Server and database running correctly');
 });
 
 app.listen(PORT, () => {
